8e6f06f797db0210ed0e74ea1207b0da5fc32292
Heather Kirk <hskirk@gmail.com>
2019-04-22 00:21:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
  end 
  
  post '/puppy' do
-   @new_puppy = Puppy.new
+   @new_puppy = Puppy.new(name :name, breed: breed, months_old: months_old)
    erb :display_puppy
  end 
  
